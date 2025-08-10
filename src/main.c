fb0e28745b2b642ecaa9a1b24b15df7d1f341bc0
0xMukesh <hey.mukesh272921@gmail.com>
2025-08-10 16:41:05 +0530

    feat: basic led on-off using button

--- src/main.c
+++ src/main.c
@@ -1,9 +1,30 @@
 #include <Arduino.h>
 
-void loop()
-{
-}
+#define LED_PIN PB0
+#define BUTTON_PIN PB1
+
+int buttonStatus = HIGH;
+int ledStatus = LOW;
 
 void setup()
 {
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+}
+
+void loop()
+{
+    int reading = digitalRead(BUTTON_PIN);
+    delay(10); // debounce filter
+
+    if (buttonStatus != reading)
+    {
+        buttonStatus = reading;
+
+        if (buttonStatus == LOW)
+        {
+            ledStatus = ledStatus == LOW ? HIGH : LOW;
+            digitalWrite(LED_PIN, ledStatus);
+        }
+    }
 }
